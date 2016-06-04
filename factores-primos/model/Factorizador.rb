403ba491d439c673906b7b56711c083aa4000d52
juliani2 <julianezequielmoreno@gmail.com>
2016-06-04 17:52:35 -0300

    deberia retornar [2,2] cuando factorizar(4)

--- factores-primos/model/Factorizador.rb
+++ factores-primos/model/Factorizador.rb
@@ -1,5 +1,21 @@
 class Factorizador
 	def factorizar(numero)
-		return numero
+		factor = 2
+		factorizacion = []
+
+		if numero < 2
+			factorizacion << numero
+		else
+			while numero >= 2 do
+				if numero%factor == 0
+					factorizacion << factor
+					numero = numero/factor
+				else
+					factor += 1
+				end
+			end
+		end
+
+		return factorizacion
 	end
 end
--- factores-primos/spec/factorizador_spec.rb
+++ factores-primos/spec/factorizador_spec.rb
@@ -5,17 +5,21 @@ describe 'Factorizador' do
 
 	it 'deberia retornar 1 cuando factorizar(1)' do
     factorizador = Factorizador.new
-    expect(factorizador.factorizar(1)).to eq 1
+    expect(factorizador.factorizar(1)).to eq [1]
   end
 
   it 'deberia retornar 2 cuando factorizar(2)' do
     factorizador = Factorizador.new
-    expect(factorizador.factorizar(2)).to eq 2
+    expect(factorizador.factorizar(2)).to eq [2]
   end
 
   it 'deberia retornar 3 cuando factorizar(3)' do
     factorizador = Factorizador.new
-    expect(factorizador.factorizar(3)).to eq 3
+    expect(factorizador.factorizar(3)).to eq [3]
   end
 
+  it 'deberia retornar [2,2] cuando factorizar(4)' do
+    factorizador = Factorizador.new
+    expect(factorizador.factorizar(4)).to eq [2,2]
+  end
 end
