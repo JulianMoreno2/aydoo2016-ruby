050cc82fbc6630d9d963fbbd7eb6f4e74f925a5b
juliani2 <julianezequielmoreno@gmail.com>
2016-06-05 19:40:33 -0300

    deberia crear salida y poner Factores primos de 2: en el archivo

--- factores-primos/spec/imprimidor_en_archivo_spec.rb
+++ factores-primos/spec/imprimidor_en_archivo_spec.rb
@@ -3,7 +3,7 @@ require_relative '../model/ImprimidorEnArchivo'
 
 describe 'ImprimidorEnArchivo' do
 
-	it 'deberia crear 'salida' y poner Factores primos de 2: 2 en el archivo' do
+	it 'deberia crear salida y poner Factores primos de 2: en el archivo' do
 		file = mock('salida')
     File.should_receive(:open).with("salida", "w").and_yield(file)
     file.should_receive(:write).with("Factores primos de 2: ")
@@ -12,5 +12,4 @@ describe 'ImprimidorEnArchivo' do
     imprimidorEnArchivo.setSalida("salida")
     imprimidorEnArchivo.imprimir(["2"])
   end
-
 end
