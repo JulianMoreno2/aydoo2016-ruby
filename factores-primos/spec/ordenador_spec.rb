8a572e70c61c299468f46a10caffc47b7d00f386
juliani2 <julianezequielmoreno@gmail.com>
2016-06-05 16:36:05 -0300

    eberia retornar [2,3] cuando ordenar([2,3])

--- factores-primos/spec/ordenador_spec.rb
+++ factores-primos/spec/ordenador_spec.rb
@@ -6,5 +6,10 @@ describe 'Ordenador' do
 	it 'deberia retornar ["2"] cuando ordenar(["2"])' do
     formateador = Ordenador.new
     expect(formateador.ordenar(["2"])).to eq ["2"]
-  end  
+  end
+
+  it 'deberia retornar ["2","3"] cuando ordenar(["2","3"])' do
+    formateador = Ordenador.new
+    expect(formateador.ordenar(["2","3"])).to eq ["2","3"]
+  end 
 end
