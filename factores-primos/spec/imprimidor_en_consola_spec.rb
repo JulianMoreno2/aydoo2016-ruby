a9e3854abcbd00a972fab212c48c8a85b36e8ce1
juliani2 <julianezequielmoreno@gmail.com>
2016-06-05 17:39:02 -0300

    deberia retornar 2 2 cuando imprimir([2 ,2 ])

--- factores-primos/spec/imprimidor_en_consola_spec.rb
+++ factores-primos/spec/imprimidor_en_consola_spec.rb
@@ -6,7 +6,7 @@ describe 'ImprimidorEnConsola' do
 	it 'deberia retornar "2" cuando imprimir([2])' do
     imprimidor_en_consola = ImprimidorEnConsola.new
     expect(imprimidor_en_consola.imprimir(["2"])).to eq "2"
-  end 
+  end
 
   it 'deberia retornar "2 2" cuando imprimir([2 ,2 ])' do
     imprimidor_en_consola = ImprimidorEnConsola.new
