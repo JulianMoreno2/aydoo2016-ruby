8b064abb0f6f0b9d677c5cc59ead90085b19ed34
juliani2 <julianezequielmoreno@gmail.com>
2016-06-05 17:22:45 -0300

    deberia retornar 2 cuando imprimir([2])

---
+++ factores-primos/model/ImprimidorEnConsola.rb
@@ -0,0 +1,6 @@
+class ImprimidorEnConsola
+	def imprimir(factores)
+		puts factores[0]
+		return "#{factores[0]}"
+	end
+end
---
+++ factores-primos/spec/imprimidor_en_consola_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec' 
+require_relative '../model/ImprimidorEnConsola'
+
+describe 'ImprimidorEnConsola' do
+	it 'deberia retornar "2" cuando imprimir([2])' do
+    imprimidor_en_consola = ImprimidorEnConsola.new
+    expect(imprimidor_en_consola.imprimir(["2"])).to eq "2"
+  end  
+end
