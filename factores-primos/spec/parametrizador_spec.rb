75f49fb13cf3f0b3de3b7ecb862edb06ad3c4550
juliani2 <julianezequielmoreno@gmail.com>
2016-06-05 03:42:05 -0300

    deberia retornar [20,pretty] cuando alinearParametros(parametros)

--- factores-primos/spec/parametrizador_spec.rb
+++ factores-primos/spec/parametrizador_spec.rb
@@ -8,8 +8,13 @@ describe 'Parametrizador' do
     expect(parametrizador.alinear_parametros(["20"])).to eq ["20"]
   end
 
-  it 'deberia retornar [20,"quiet"] cuando alinearParametros(parametros)' do
+  it 'deberia retornar ["20","quiet"] cuando alinearParametros(parametros)' do
     parametrizador = Parametrizador.new
     expect(parametrizador.alinear_parametros(["20","--format=quiet"])).to eq ["20","quiet"]
+  end
+
+  it 'deberia retornar ["20","pretty"] cuando alinearParametros(parametros)' do
+    parametrizador = Parametrizador.new
+    expect(parametrizador.alinear_parametros(["20","--format=pretty"])).to eq ["20","pretty"]
   end
 end
