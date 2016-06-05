dcc142984b4a2755c405b352bf18323c03a1f192
juliani2 <julianezequielmoreno@gmail.com>
2016-06-05 11:59:32 -0300

    deberia retornar [20,pretty,asc,] cuando alinearParametros(20,--format=pretty,--sort:asc)

--- factores-primos/model/Parametrizador.rb
+++ factores-primos/model/Parametrizador.rb
@@ -2,11 +2,26 @@ class Parametrizador
 
 	def alinear_parametros(parametros)
 
-		if parametros.length > 1
-			opcion = parametros[1].split("=")
-			parametros[1] = opcion[1]
-		end
-		return parametros
-	end
+		parametros_alineados = [parametros[0],"","",""]
 
+		for opcion in 1...parametros.length do
+			
+			opcionConIgual = parametros[opcion].split("=")
+			opcionConPuntos = parametros[opcion].split(":")
+
+			if opcionConIgual.length > 1
+				
+				if opcionConIgual[0] == "--format"
+					parametros_alineados[1] = opcionConIgual[1]
+				elsif opcionConIgual[0] == "--output-file"
+					parametros_alineados[3] = opcionConIgual[1]
+				end	
+
+			elsif opcionConPuntos.length > 1
+				parametros_alineados[2] = opcionConPuntos[1]
+			end				
+		end
+
+		return parametros_alineados
+	end
 end
--- factores-primos/spec/parametrizador_spec.rb
+++ factores-primos/spec/parametrizador_spec.rb
@@ -3,18 +3,24 @@ require_relative '../model/Parametrizador'
 
 describe 'Parametrizador' do
 
-	it 'deberia retornar ["20"] cuando alinearParametros(parametros)' do
+	it 'deberia retornar ["20","","",""] cuando alinearParametros("20")' do
     parametrizador = Parametrizador.new
-    expect(parametrizador.alinear_parametros(["20"])).to eq ["20"]
+    expect(parametrizador.alinear_parametros(["20"])).to eq ["20","","",""]
   end
 
-  it 'deberia retornar ["20","quiet"] cuando alinearParametros(parametros)' do
+  it 'deberia retornar ["20","quiet","",""] cuando alinearParametros("20","--format=quiet")' do
     parametrizador = Parametrizador.new
-    expect(parametrizador.alinear_parametros(["20","--format=quiet"])).to eq ["20","quiet"]
+    expect(parametrizador.alinear_parametros(["20","--format=quiet"])).to eq ["20","quiet","",""]
   end
 
-  it 'deberia retornar ["20","pretty"] cuando alinearParametros(parametros)' do
+  it 'deberia retornar ["20","pretty","",""] cuando alinearParametros("20","--format=pretty")' do
     parametrizador = Parametrizador.new
-    expect(parametrizador.alinear_parametros(["20","--format=pretty"])).to eq ["20","pretty"]
+    expect(parametrizador.alinear_parametros(["20","--format=pretty"])).to eq ["20","pretty","",""]
   end
+
+  it 'deberia retornar ["20","pretty","asc",""] cuando alinearParametros("20","--format=pretty","--sort:asc")' do
+    parametrizador = Parametrizador.new
+    expect(parametrizador.alinear_parametros(["20","--format=pretty","--sort:asc"])).to eq ["20","pretty","asc",""]
+  end
+
 end
