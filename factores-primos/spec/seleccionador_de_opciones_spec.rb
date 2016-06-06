600a64beaa450327c62095eb627d2d8c52c3b94a
juliani2 <julianezequielmoreno@gmail.com>
2016-06-05 22:43:44 -0300

    deberia retornar 'Factores primos de 4: 2 2 ' cuando ejecutar(['4','--format=pretty'])

--- factores-primos/spec/seleccionador_de_opciones_spec.rb
+++ factores-primos/spec/seleccionador_de_opciones_spec.rb
@@ -3,9 +3,14 @@ require_relative '../model/SeleccionadorDeOpciones'
 
 describe 'SeleccionadorDeOpciones' do
 
-	it 'deberia retornar "Factores primos de 2: 2 2 " cuando ejecutar(["2"])' do
+	it 'deberia retornar "Factores primos de 4: 2 2 " cuando ejecutar(["4"])' do
     seleccionador = SeleccionadorDeOpciones.new
     expect(seleccionador.ejecutar(["4"])).to eq "Factores primos de 4: 2 2 "
   end  
+
+  it 'deberia retornar "Factores primos de 4: 2 2 " cuando ejecutar(["4","--format=pretty"])' do
+    seleccionador = SeleccionadorDeOpciones.new
+    expect(seleccionador.ejecutar(["4"])).to eq "Factores primos de 4: 2 2 "
+  end 
 
 end
